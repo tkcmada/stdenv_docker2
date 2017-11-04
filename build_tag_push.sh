a6c5cabc3a1ac4065c4ae2ad2cc246b80fdb1c65
Tokutaro Shimada <tokutaro@gmail.com>
2017-11-04 10:57:29 +0900

    adding build arg to build_tag_push.sh

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -1,3 +1,6 @@
-docker build -t $1 $1 \
+docker build \
+  --build-arg RHEL_USER=$RHEL_USER \
+  --build-arg RHEL_PASSWORD=$RHEL_PASSWORD \
+  -t $1 $1 \
 && docker tag $1 localhost:5000/$1 \
 && docker push localhost:5000/$1
