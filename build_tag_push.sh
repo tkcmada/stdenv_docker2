06993533b8822e36963ecd23c8f3c4574f9f0e7c
Tokutaro Shimada <tokutaro@gmail.com>
2017-11-05 09:43:12 +0900

    fixed build_tag_push.sh

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -5,5 +5,7 @@ sudo sh retry.sh docker build \
   --build-arg RHEL_USER=$RHEL_USER \
   --build-arg RHEL_PASSWORD=$RHEL_PASSWORD \
   -t $1 $1
-sudo sh retry.sh docker tag $1 localhost:5000/$1 \
+
+sudo sh retry.sh docker tag $1 localhost:5000/$1
+
 sudo sh retry.sh docker push localhost:5000/$1
