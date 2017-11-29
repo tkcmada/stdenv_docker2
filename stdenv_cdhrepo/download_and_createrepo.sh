2b40db99b170b38345a16b95daa08c7f7df45bb5
Tokutaro Shimada <tokutaro@gmail.com>
2017-11-30 00:47:36 +0900

    adding cdsw

--- stdenv_cdhrepo/download_and_createrepo.sh
+++ stdenv_cdhrepo/download_and_createrepo.sh
@@ -8,9 +8,9 @@ test "$reponame" = "" && exit 1
 
 cd ~ || exit 1
 reposync -r $reponame || exit 1
-sudo mkdir -p $repodir || exit 1
-find $reponame -name '*.rpm' | sudo xargs mv -t $repodir/ || exit 1
-sudo createrepo $repodir || exit 1
+mkdir -p $repodir || exit 1
+find $reponame -name '*.rpm' | xargs mv -t $repodir/ || exit 1
+createrepo $repodir || exit 1
 rm -rf $reponame || exit 1
 
 
